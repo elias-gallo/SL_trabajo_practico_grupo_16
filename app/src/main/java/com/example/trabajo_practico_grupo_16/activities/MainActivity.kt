61130a3326de60404b5d1025535f8678620f8f66
elias-gallo <eliasgallounla@gmail.com>
2024-10-21 13:33:30 -0300

    Agrego notificación cuando se marca el CheckBox

--- app/src/main/java/com/example/trabajo_practico_grupo_16/activities/MainActivity.kt
+++ app/src/main/java/com/example/trabajo_practico_grupo_16/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.trabajo_practico_grupo_16.activities
 
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
@@ -9,6 +13,9 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.trabajo_practico_grupo_16.database.AppDataBase
@@ -73,6 +80,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intentRegister)
             finish()
         }
+
+
+        cbRecordar.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                crearNotificacion()
+            }
+        }
     }
 
     private fun iniciarSesion(email: String, password: String) {
@@ -97,6 +111,29 @@ class MainActivity : AppCompatActivity() {
         intentHome.putExtra(resources.getString(R.string.nombre_usuario), usuario)
         startActivity(intentHome)
         finish()
+    }
+
+    private fun crearNotificacion() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "checkbox_channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, "CheckBox Channel", importance).apply {
+                description = "Canal para notificaciones del CheckBox"
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.baseline_check_24)
+            .setContentTitle("Notificación de CheckBox")
+            .setContentText("Su usuario será recordado")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, notificationBuilder.build())
+        }
     }
 
 }
